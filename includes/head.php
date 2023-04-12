eb1aeac6b53b9bba454f6d73101aea0cd30ef7c2
OAuthority <lukerigbx@icloud.com>
2023-04-12 01:25:06 +0100

    creating basic landing page for the interim

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="utf-8" />
+    <title>WikiForge</title>
+    <link rel="stylesheet" href="/css/index.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="keywords" content="wikiforge, free, ad-free, ad free, no ads, without ads, wiki hosting, free wiki hosting, wiki hosting sites, wiki farms, sign up, mediawiki, mediawiki hosting, open source, donate" />
+    <meta name="description" content="WikiForge is an ad-free, premium, and community-centric wiki hosting platform where anyone can create a free MediaWiki-powered wiki. Request your free wiki today!" />
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+    <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+    <link rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/" />
+</head>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <?php include_once __DIR__ . '/includes/head.php'; ?>
+    <body>
+    <nav id="wfheader">
+        <img class="logo" src="/images/WikiForge_Wordmark.svg" alt="WikiForge">
+    </nav>
+    <section class="basicSection">
+        <!-- basic grid -->
+        <div class="introHeader">
+            <div class="introHeading">Welcome to WikiForge.</div>
+            <div class="introImages">
+                <div id="meta">
+                    <img class="meta" src="/images/Meta%20Main%20Page.png" alt="Meta Main Page" />
+                </div>
+            </div>
+        </div>
+        <h2 class="featuresHeader">Features</h2>
+        <!-- features grid -->
+        <div class="features">
+            <div class="mediawiki">
+                <img class="featuresGrid" width="50" height="50" src="/images/Server.svg" />
+                <div class="featuresGridInfo">
+                    We're running the most up-to-date version of MediaWiki, and are committed to updating whenever there is a new stable version available.
+                </div>
+            </div>
+            <div class="support">
+                <img class="featuresGrid" width="50" height="50" src="/images/Customize.svg" />
+                <div class="featuresGridInfo">
+                    Chose from hundreds of extensions and skins to customise your wiki the way you like it. Take advantage of our intuitive user interface to modify extensions, permissions, and other settings.
+                </div>
+            </div>
+            <div class="community">
+                <img class="featuresGrid" width="50" height="50" src="/images/Community.svg" />
+                <div class="featuresGridInfo">
+                   Get quick support from our expert volunteers who have years of experience with the MediaWiki software. Also take advantage of our growing community who are always willing to help!
+                </div>
+            </div>
+            <div class="customise">
+                <img class="featuresGrid" width="50" height="50" src="/images/Responsive.svg" />
+                <div class="featuresGridInfo">
+                    All of our wikis support using MobileFrontend which makes the UI mobile friendly. This is enabled by default, however, we also allow you to switch this off by disabling it.
+                </div>
+            </div>
+            <div class="premium">
+                <img class="featuresGrid" width="50" height="50" src="/images/GitHub.svg" />
+                <div class="featuresGridInfo">
+                    We're an open source platform! This means that you can check out all of our code and how we do things over on GitHub. Pull requests are welcome!
+                </div>
+            </div>
+            <div class="opensource">
+                <img class="featuresGrid" width="50" height="50" src="/images/Free.svg" />
+                <div class="featuresGridInfo">
+                    Best of all, we're free. We'll always offer free hosting for those that need it.
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="footer">
+        <footer id="footer">
+            WikiForge<br>
+            Except where otherwise noted, the content of this site is licensed under a Attribution-ShareAlike 4.0 International (CC BY-SA 4.0).
+        </footer>
+
+    </section>
+    </body>
+</html>
