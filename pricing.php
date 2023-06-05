c1455dd6d58e5730e7a62a07553cf6fcec3690f1
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-05 13:27:27 -0600

    Add pricing page

---
+++ pricing.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WikiForge Pricing</title>
+    <link rel="stylesheet" type="text/css" href="css/pricing.css">
+</head>
+<body>
+    <div class="container">
+        <h1>WikiForge Pricing</h1>
+        <div id="pricingTable"></div>
+    </div>
+
+    <script src="js/pricing.js"></script>
+</body>
+</html>
