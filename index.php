136383e8b08726506a3375bb4c279cca7aed744d
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-05 16:35:21 -0600

    Add

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 						<a href="https://meta.wikiforge.net/wiki/Special:RequestPremiumWiki" class="button xl main mt-lg w-button">
 							Start Your Wiki Journey Today
 						</a>
-						<a href="pricing.php" class="pricing-button pricing-link mt-lg">
+						<a href="pricing.php" class="pricing-button pricing-link mt-lg max-w-lg">
 							See Pricing
 						</a>
 					</div>
