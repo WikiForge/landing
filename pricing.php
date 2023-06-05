6270019d3ec6f419b0afe9fbc2ef25e3c994fd20
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-05 13:36:38 -0600

    Update

--- pricing.php
+++ pricing.php
@@ -1,15 +1,49 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>WikiForge Pricing</title>
-    <link rel="stylesheet" type="text/css" href="css/pricing.css">
-</head>
-<body>
-    <div class="container">
-        <h1>WikiForge Pricing</h1>
-        <div id="pricingTable"></div>
-    </div>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>WikiForge Pricing</title>
+		<meta content="width=device-width, initial-scale=1" name="viewport" />
+		<meta name="description" content="Create, manage, and collaborate with ease on wikis, powered by WikiForge – the ultimate wiki farm for all your knowledge-sharing needs. Try our premium wiki hosting today." />
+		<meta name="twitter:description" content="Create, manage, and collaborate with ease on wikis, powered by WikiForge – the ultimate wiki farm for all your knowledge-sharing needs. Try our premium wiki hosting today." />
+		<meta property="og:description" content="Create, manage, and collaborate with ease on wikis, powered by WikiForge – the ultimate wiki farm for all your knowledge-sharing needs. Try our premium wiki hosting today." />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="WikiForge" />
+		<meta property="og:site_name" content="WikiForge" />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:title" content="WikiForge" />
+		<meta name="twitter:site" content="@WikiForge" />
+		<meta name="mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="application-name" content="WikiForge" />
+		<meta name="apple-mobile-web-app-title" content="WikiForge" />
+		<link rel="manifest" href="/manifest.json" />
+		<link rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/" />
+		<link href="/css/normalize.css" rel="stylesheet" type="text/css" />
+		<link href="/css/webflow.css" rel="stylesheet" type="text/css" />
+		<link href="/css/main.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="css/pricing.css">
+		<!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+		<script type="text/javascript">
+			!(function (o, c) {
+				var n = c.documentElement,
+					t = " w-mod-";
+				(n.className += t + "js"),
+				("ontouchstart" in o ||
+					(o.DocumentTouch && c instanceof DocumentTouch)) &&
+				(n.className += t + "touch");
+			})(window, document);
+		</script>
+		<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+		<script type="application/ld+json">[{"@context":"https:\/\/schema.org","@type":"WebPage","headline":"WikiForge","url":"/","thumbnailUrl":"","dateCreated":"","creator":[],"keywords":[]}]</script>
+	</head>
+	<body>
+		<div class="container">
+			<h1>WikiForge Pricing</h1>
+			<div id="pricingTable"></div>
+		</div>
 
-    <script src="js/pricing.js"></script>
-</body>
+		<script src="js/pricing.js"></script>
+	</body>
 </html>
